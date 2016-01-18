07a0281ceddd163e5573dd7873fea1e9e8dc7b92
Tyler Sorenson <Tyler Sorenson>
2016-01-17 19:32:46 -0700

    Update Homepage

---
+++ homepage.js
@@ -0,0 +1,20 @@
+function loadDoc(file, type){
+	var xhttp = new XMLHttpRequest();
+	console.log(file + " where is it");
+	xhttp.onreadystatechange = function() {
+		if (xhttp.readyState == 4 && xhttp.status == 200) {
+			document.getElementById("accomp1Detail").innerHTML = text;
+			//parse1(xhttp.responseText);
+		}
+  	};
+  	xhttp.open("GET", file, true);
+  	xhttp.send();
+}
+
+function parse1(text) {
+	document.getElementById("accomp1Detail").innerHTML = text;
+}
+
+function parse2(text) {
+	document.getElementById("accomp2Detail").innerHTML = text;
+}
