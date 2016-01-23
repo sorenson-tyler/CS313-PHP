e2c7d7f5b3d5f1a8766218e52e40d3db4c227e14
Tyler Sorenson <Tyler Sorenson>
2016-01-22 22:49:28 -0700

    Survey

--- surveyresults.php
+++ surveyresults.php
@@ -1,15 +1,22 @@
 <?php
     session_start();
+    $_SESSION["firstname"] = $_POST["first_name"];
+    $_SESSION["lastname"] = $_POST["last_name"];
+    include 'results.php';
 ?>
 <!DOCTYPE html>
 <html>
     <body>
         <h3>Thank you for your response 
         <?php
-            $_SESSION["firstname"] = $_POST["first_name"];
-            $_SESSION["lastname"] = $_POST["last_name"];
             echo $_SESSION["firstname"];
-        ?>
+        ?> 
+            you drive a
+            <?php
+                echo $_POST["car_color"] + " ";
+                echo $_POST["car_model"] + " ";
+                echo $_POST["car_type"];
+            ?>
         </h3>
     </body>
 </html>
