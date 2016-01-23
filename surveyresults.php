33f4faadf3039a2a822cd8d8b72a3052bd8d846c
Tyler Sorenson <Tyler Sorenson>
2016-01-22 22:59:15 -0700

    Survey

--- surveyresults.php
+++ surveyresults.php
@@ -5,14 +5,28 @@
 ?>
 <!DOCTYPE html>
 <html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="survey.css"/>
+        <link rel="stylesheet" type="text/css" href="homepage.css"/>
+    </head>
     <body>
+        <div id="header">
+            <h1 id="name">Tyler Sorenson</h1>
+            <nav id="navigation">
+                <ul>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="https://www.byui.edu">BYU - Idaho</a></li>
+                    <li><a href="index.html">About</a></li>
+                </ul>
+            </nav>
+        </div>
         <h3>Thank you for your response 
         <?php
             $color = $_POST["car_color"];
             $model = $_POST["car_model"];
             $type = $_POST["car_type"];
             echo $_SESSION["firstname"];
-            echo " you drive a $color $model $type.";
+            echo ", you drive a $color $model $type.";
         ?>
         </h3>
     </body>
