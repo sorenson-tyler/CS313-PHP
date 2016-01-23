a9e8bd4945824404bf2ee1cc7d45ef2a1fbc12d5
Tyler Sorenson <Tyler Sorenson>
2016-01-22 22:23:13 -0700

    Survey

--- surveyresults.php
+++ surveyresults.php
@@ -6,8 +6,8 @@
     <body>
         <?php
             // Set session variables
-            $_SESSION["firstname"] = $_POST["firstname"];
-            $_SESSION["lastname"] = $_POST["lastname"];
+            $_SESSION["firstname"] = $_POST["first_name"];
+            $_SESSION["lastname"] = $_POST["last_name"];
         ?>
         <h1>Your name is 
             <?php echo $_SESSION['firstname'];?>
