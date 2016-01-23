c6215cf8544a1cd14612d2072353f124e047007c
Tyler Sorenson <Tyler Sorenson>
2016-01-22 23:04:42 -0700

    Survey

--- results.php
+++ results.php
@@ -1,28 +1,28 @@
 <?php
-//Make
-$Honda = '0';
-$Toyota = '0';
-$Nissan = '0';
-$Ford = '0';
-$Jeep = '0';
-$Buick = '0';
-$Chevrolet = '0';
-$Other = '0';
-$None = '0';
+    //Make
+    $Honda = '0';
+    $Toyota = '0';
+    $Nissan = '0';
+    $Ford = '0';
+    $Jeep = '0';
+    $Buick = '0';
+    $Chevrolet = '0';
+    $Other = '0';
+    $None = '0';
 
-//Model
-$Sedan = '0';
-$Minivan = '0';
-$Truck = '0';
-$SUV = '0';
-$Sports car = '0';
+    //Model
+    $Sedan = '0';
+    $Minivan = '0';
+    $Truck = '0';
+    $SUV = '0';
+    $Sports car = '0';
 
-//Color
-$Red = '0';
-$Blue = '0';
-$Green = '0';
-$Black = '0';
-$White = '0';
-$Grey = '0';
-$Other = '0';
+    //Color
+    $Red = '0';
+    $Blue = '0';
+    $Green = '0';
+    $Black = '0';
+    $White = '0';
+    $Grey = '0';
+    $Other = '0';
 ?>
--- surveyresults.php
+++ surveyresults.php
@@ -2,6 +2,7 @@
     session_start();
     $_SESSION["firstname"] = $_POST["first_name"];
     $_SESSION["lastname"] = $_POST["last_name"];
+    include 'results.php';
 ?>
 <!DOCTYPE html>
 <html>
