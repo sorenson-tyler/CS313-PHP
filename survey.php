b654f59402b5f0d4124851abd15013ac909c4888
Tyler Sorenson <Tyler Sorenson>
2016-01-24 12:08:33 -0700

    Redirect

--- survey.php
+++ survey.php
@@ -31,7 +31,7 @@
         <div class="title">
             <h1 id="title">Student Survey</h1>
         </div>
-        <button id="results_button" href="surveyresults.php">Click to see Results</button>
+        <button id="results_button" href="http://php-tsorenson/surveyresults.php">Click to see Results</button>
         <form action="surveyresults.php" method="post">
             <div id="firstname">
                 First name: 
