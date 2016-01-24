8a464031b46360c718f84b17a8c29dcd96e0bcc2
Tyler Sorenson <Tyler Sorenson>
2016-01-24 12:06:08 -0700

    Redirect

--- survey.js
+++ survey.js
@@ -3,7 +3,6 @@ function loadResults(file){
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
             document.getElementById("first").innerHTML = xhttp.responseText;
-			}
 		}
   	};
   	xhttp.open("GET", file, true);
