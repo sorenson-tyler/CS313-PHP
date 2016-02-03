f14316dcc4d247dad3ef415f9cecf80473f693be
Tyler Sorenson <tysoren41@gmail.com>
2016-02-02 19:05:47 -0700

    Database

--- accessDB.php
+++ accessDB.php
@@ -7,6 +7,19 @@
         <form id = "gameSearch">
             Search for game:
             <input name = "searchBox" type=text width="20px">
-            <button id="searchButton"/>
+            <button id="searchButton" name="Search"/>
         </form>
+        <?php
+              try
+            {
+               $user = 'php'
+               $password = 'dstasl66'; 
+               $db = new PDO('mysql:host=127.0.0.1;dbname=localhost', $user, $password);
+            }
+            catch (PDOException $ex) 
+            {
+               echo 'Error!: ' . $ex->getMessage();
+               die(); 
+            }
+        ?>
     </body>
