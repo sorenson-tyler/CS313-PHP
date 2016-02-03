71d706ef6436d66eed67c7b32315c89c36a7b982
Tyler Sorenson <tysoren41@gmail.com>
2016-02-02 19:02:18 -0700

    Database

--- accessDB.php
+++ accessDB.php
@@ -9,17 +9,4 @@
             <input name = "searchBox" type=text width="20px">
             <button id="searchButton"/>
         </form>
-        <?php
-            try
-            {
-               $user = 'php'
-               $password = 'php-pass'; 
-               $db = new PDO('mysql:host=127.0.0.1;dbname=php', $user, $password);
-            }
-            catch (PDOException $ex) 
-            {
-               echo 'Error!: ' . $ex->getMessage();
-               die(); 
-            }
-        ?>
     </body>
