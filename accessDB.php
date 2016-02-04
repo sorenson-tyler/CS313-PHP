65ac39d037b06706ba76ef557891522f4c802b58
Tyler Sorenson <tysoren41@gmail.com>
2016-02-03 19:50:11 -0700

    Database

--- accessDB.php
+++ accessDB.php
@@ -1,44 +1,20 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        
-    </head>
-    <body>        
-        <?php
-            if(isset($_POST["searchBox"]))
-            {
-                try
-                {
-                   $user = 'php';
-                   $password = 'dstasl66'; 
-                   $db = new PDO('mysql:host=127.6.94.2;dbname=php', $user, $password);
-                    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    echo "Connected successfully"; 
-                }
-                catch (PDOException $ex) 
-                {
-                   echo 'Error!: ' . $ex->getMessage();
-                   die(); 
-                }
-                
-                $search = $_POST["searchBox"];
-                echo $search;
+<?php
+    try
+    {
+       $user = 'root';
+       $password = ''; 
+       $db = new PDO('mysql:host=localhost;dbname=php', $user, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully"; 
+    }
+    catch (PDOException $ex) 
+    {
+       echo 'Error!: ' . $ex->getMessage();
+       die(); 
+    }
 
-                $stmt = $db->prepare("SELECT * FROM table game_type where name = '"$search"'");
-                $stmt->execute();
-                $result = $stmt->fetchAll();
-                echo $result;
-            }
-            else
-            {
-        ?>
-            <form id = "gameSearch" method="post" action="accessDB.php">
-                Search for game:
-                <input name = "searchBox" type=text width="20px">
-                <submit id="searchButton" type="submit" name="Search">Search</submit>
-            </form>
-        <?php
-            }
-        ?>
-    </body>
-</html>
+    $stmt = $db->prepare("SELECT * FROM table game_type where name = '"$search"'");
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    print_r $result;
+?>
