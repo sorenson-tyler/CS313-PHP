2fab39a179792d0d4dce844f124ab5e624096a7b
Tyler Sorenson <Tyler Sorenson>
2016-02-06 12:20:44 -0700

    Database

--- databaseaccess.php
+++ databaseaccess.php
@@ -1,21 +1,8 @@
 <!DOCTYPE html>
-<?php
-    $dbHost = getenv('OPENSHIFT_MYSQL_DB_HOST');
-    $dbPort = getenv('OPENSHIFT_MYSQL_DB_PORT');
-    $dbUser = getenv('OPENSHIFT_MYSQL_DB_USERNAME');
-    $dbPassword = getenv('OPENSHIFT_MYSQL_DB_PASSWORD');
-    $dbName = 'php';
-    echo "host:$dbHost:$dbPort dbName:$dbName user:$dbUser password:$dbPassword<br />\n";
 
-    try
-    {
-        $db = new PDO("mysql:host=$dbHost:$dbPort;dbname=$dbName", $dbUser, $dbPassword);
-    }
-    catch (PDOException $ex)
-    {
-        echo 'Error!: ' . $ex->getMessage();
-        die(); 
-    }
+<?php
+    require 'connectDB.php';
+
     $name = "Monopoly";
     $stmt = $db->prepare('SELECT * FROM game_type WHERE name=:name');
     $stmt->execute(array(':name' => $name));
