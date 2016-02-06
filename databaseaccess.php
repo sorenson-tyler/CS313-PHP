d421f2337bd8b12cc6c09fcf28541c2d0a567c14
Tyler Sorenson <Tyler Sorenson>
2016-02-06 12:32:20 -0700

    Database

--- databaseaccess.php
+++ databaseaccess.php
@@ -1,11 +1,10 @@
 <!DOCTYPE html>
+    <?php
+        require 'connectDB.php';
 
-<?php
-    require 'connectDB.php';
-
-    $name = "Monopoly";
-    $stmt = $db->prepare('SELECT * FROM game_type WHERE name=:name');
-    $stmt->execute(array(':name' => $name));
-    $rows = $stmt->fetchAll();
-    echo 'Name: ' . $rows[0]['name'] . 'Min players: ' . $rows[0]['number_players_min'] . 'Max players: ' . $rows[0]['number_players_max'] . 'Highest score wins: ' . $rows[0]['positive_scoring']; 
-?>
+        $name = $REQUEST['name'];
+        $stmt = $db->prepare('SELECT * FROM game_type WHERE name=:name');
+        $stmt->execute(array(':name' => $name));
+        $rows = $stmt->fetchAll();
+        echo 'Name: ' . $rows[0]['name'] . 'Min players: ' . $rows[0]['number_players_min'] . 'Max players: ' . $rows[0]['number_players_max'] . 'Highest score wins: ' . $rows[0]['positive_scoring']; 
+    ?>
