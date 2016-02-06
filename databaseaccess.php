33a92d15e601ad1f46542989e252ed72393ad8fc
Tyler Sorenson <Tyler Sorenson>
2016-02-06 12:14:09 -0700

    Database

--- databaseaccess.php
+++ databaseaccess.php
@@ -20,5 +20,5 @@
     $stmt = $db->prepare('SELECT * FROM game_type WHERE name=:name');
     $stmt->execute(array(':name' => $name));
     $rows = $stmt->fetchAll();
-    echo 'Name: ' . $rows[0]['name'] . 'Min players: ' . $rows[0]['number_players_min'] . 'Max players: ' . $rows[0]['number_players_max' . 'Highest score wins: ' . $rows[0]['positive_scoring'] '<br>'; 
+    echo 'Name: ' . $rows[0]['name'] . 'Min players: ' . $rows[0]['number_players_min'] . 'Max players: ' . $rows[0]['number_players_max' . 'Highest score wins: ' . $rows[0]['positive_scoring']; 
 ?>
