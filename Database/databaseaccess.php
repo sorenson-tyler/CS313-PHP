1a93aa97c4ad1d5412d6f52717757f1b8f115b6f
Tyler Sorenson <Tyler Sorenson>
2016-02-06 18:45:36 -0700

    Database

--- Database/databaseaccess.php
+++ Database/databaseaccess.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
     <?php
-        require 'connectDB.php';
+        require 'php-tsorenson.rhcloud.com/connectDB.php';
 
         $name = $_POST['name'];
         $stmt = $db->prepare('SELECT * FROM game_type WHERE name=:name');
