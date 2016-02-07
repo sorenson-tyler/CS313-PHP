c14e30059cff2e900a6270ad7be885beb438ae56
Tyler Sorenson <Tyler Sorenson>
2016-02-07 12:38:51 -0700

    Database

--- addUser.php
+++ addUser.php
@@ -1,7 +1,7 @@
 <?php
     $user_name = $_POST['name'];
 
-    if ($game_name != null)
+    if ($user_name != null)
         echo "User added";
     else
         echo "Please fill in all items and try again";
