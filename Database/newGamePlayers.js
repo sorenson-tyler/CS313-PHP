8d65f1b20b8c4224c7aaf475390c1a46b3120d9c
Tyler Sorenson <Tyler Sorenson>
2016-02-08 21:06:23 -0700

    Database

--- Database/newGamePlayers.js
+++ Database/newGamePlayers.js
@@ -9,7 +9,7 @@ function addPlayers(id) {
         while (container.hasChildNodes()) {
             container.removeChild(container.lastChild);
         }
-        for (int i = 0;i < players;i++) {
+        for (i = 0;i < players;i++) {
             var input = document.createElement("input");
             input.type = "Text";
             input.name = "player" + i;
