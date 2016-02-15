1d8828bd9b2248fb672e519a402fd629f49500ba
Tyler <tysoren41@gmail.com>
2016-02-15 16:34:30 -0700

    Login pages

--- Database/addUser.php
+++ Database/addUser.php
@@ -1,11 +1,12 @@
 <?php
     require 'connectDB.php';
     $user_name = $_POST['name'];
+    $password = $_POST['password'];
 
     if ($user_name != null)
     {
-        $stmt = $db->prepare('INSERT INTO user (name) VALUES (:name)');
-        $stmt->execute(array(':name' => $user_name));
+        $stmt = $db->prepare('INSERT INTO user (name, password) VALUES (:name, :password)');
+        $stmt->execute(array(':name' => $user_name, ':password' => $password));
         echo "User added";
     }
     else
