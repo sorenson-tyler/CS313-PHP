863e1af04fd8c479484159f85ae970231d244630
Tyler <tysoren41@gmail.com>
2016-02-15 14:07:47 -0700

    Insert New Game Type

--- Database/addDatabase.php
+++ Database/addDatabase.php
@@ -7,7 +7,11 @@
     $game_positive_scoring = $_POST['positive_scoring'];
 
     if ($game_name != null && $game_min_players != null && $game_max_players != null)
+    {
+        $stmt = $db->prepare('INSERT INTO game_type (number_players_min, number_players_max, positive_scoring, name) VALUES (:min, :max, :score, :name)');
+        $stmt->execute(array(':min' => $game_min_players, ':max' => $game_max_players, ':score' => $game_positive_scoring, ':name' => $game_name));
         echo "Game type added";
+    }
     else
         echo "Please fill in all items and try again";
 ?>
