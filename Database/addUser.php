ad912327f21112f3a41a923269aaf68a31c01c93
Tyler <tysoren41@gmail.com>
2016-02-15 14:16:25 -0700

    Add User to DB

--- Database/addUser.php
+++ Database/addUser.php
@@ -1,8 +1,13 @@
 <?php
+    require 'connectDB.php';
     $user_name = $_POST['name'];
 
     if ($user_name != null)
+    {
+        $stmt = $db->prepare('INSERT INTO user (name) VALUES (:name)');
+        $stmt->execute(array(':name' => $user_name));
         echo "User added";
+    }
     else
         echo "Please fill in all items and try again";
 ?>
