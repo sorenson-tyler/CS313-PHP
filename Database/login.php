3206f5de6e79752e4c7bf9d31f52aae50ece75dc
Tyler <tysoren41@gmail.com>
2016-02-15 16:14:33 -0700

    Login pages

---
+++ Database/login.php
@@ -0,0 +1,21 @@
+<?php
+    require 'connectDB.php';
+    
+    $username = $_POST['name'];
+    $password = $_POST['password'];
+    
+    if ($username != null)
+    {
+        $stmt = $db->prepare('SELECT password FROM users WHERE name=:name');
+        $stmt->execute(array(':name' => $username));
+        $rows = $stmt->fetchAll();
+        if($rows == null)
+            echo "ERROR: No user by that name in the database!";
+        else if ($rows[0]['password'] != $password)
+            echo "ERROR: Invalid password!";
+        else if ($rows[0]['password'] == $password)
+            echo "Welcome $username";
+    }
+    else
+        echo "ERROR: Please fill in all items and try again";
+?>
