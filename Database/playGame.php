f7c764888deb3ab37d5f5d41f8d2163c4cf34e3f
Tyler <tysoren41@gmail.com>
2016-02-18 23:16:23 -0700

    Fixed redirecting page

--- Database/playGame.php
+++ Database/playGame.php
@@ -1,6 +1,6 @@
 <?php
     session_start();
-    if(isset($_SESSION["user"]))
+    if(!isset($_SESSION["user"]))
     {
         header("Location: http://php-tsorenson.rhcloud.com/Database/actions.html");
         exit;
