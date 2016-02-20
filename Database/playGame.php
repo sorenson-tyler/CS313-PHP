e4481e933bdac0754fb371d850c02fa65532b820
Tyler <tysoren41@gmail.com>
2016-02-20 16:00:43 -0700

    gfi

--- Database/playGame.php
+++ Database/playGame.php
@@ -33,12 +33,11 @@
     }
     else if ($button_clicked == "End Game")
     {
-        foreach ($users as $username)
+        foreach ($username as $user)
         {
-            $player = $username['name'];
             //End Game Query
             $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = 0, games_played = games_played + 1 WHERE name = :username');
-            $stmt->execute(array(':username' => $player));
+            $stmt->execute(array(':username' => $user));
         }
         //Redirect back to the actions page
         header("Location: http://php-tsorenson.rhcloud.com/Database/actions.php");
