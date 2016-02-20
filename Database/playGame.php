aeecb02f72504b09c7f462363b56dc8f4c8b3978
Tyler <tysoren41@gmail.com>
2016-02-20 15:35:36 -0700

    Updated db query

--- Database/playGame.php
+++ Database/playGame.php
@@ -8,21 +8,31 @@
     require 'connectDB.php';
     
     $button_clicked = $_POST['nextButton'];
-    $points = $_POST['points'];
-    $username = "Tyler";
+    $points[] = $_POST['points[]'];
+    $users = $db->query("SELECT * FROM user");
+    $username[] = null;
+    $i = 0;
+    foreach ($users as $user)
+    {
+        $username[$i++] = $user;
+    }
     
     if ($button_clicked == "Next Round")
     {
-        //Current Points and Total Points Querey
-        $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = current_score + :points WHERE name = :name');
-        $stmt->execute(array(':points' => $points, ':name' => $username));
+        foreach ($_POST['points'] as $points)
+        {
+            //Current Points and Total Points Query
+            $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = current_score + :points WHERE name = :name');
+            $stmt->execute(array(':points' => $points, ':name' => $username[$i]));
+            $i++;
+        }
         //Redirect back to the game
         header("Location: http://php-tsorenson.rhcloud.com/Database/gameForm.php");
         exit;
     }
     else if ($button_clicked == "End Game")
     {
-        //End Game Querey
+        //End Game Query
         $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = 0, games_played = games_played + 1 WHERE name = :username');
         $stmt->execute(array(':username' => $username));
         //Redirect back to the actions page
