fab9f8e6b37ba0237527832b3e8397acced897ea
Tyler <tysoren41@gmail.com>
2016-02-20 15:47:10 -0700

    Fix

--- Database/playGame.php
+++ Database/playGame.php
@@ -33,9 +33,8 @@
     }
     else if ($button_clicked == "End Game")
     {
-        foreach ($users as $user)
+        foreach ($users['name'] as $username)
         {
-            $username = $user['name'];
             //End Game Query
             $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = 0, games_played = games_played + 1 WHERE name = :username');
             $stmt->execute(array(':username' => $username));
