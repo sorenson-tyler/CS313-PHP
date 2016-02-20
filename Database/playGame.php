7bf64cb4bfbdf18c1af3fbee6fbc5d1b3e90fae4
Tyler <tysoren41@gmail.com>
2016-02-20 15:53:13 -0700

    fix

--- Database/playGame.php
+++ Database/playGame.php
@@ -35,9 +35,10 @@
     {
         foreach ($users as $username)
         {
+            $player = $username['name'];
             //End Game Query
             $stmt = $db->prepare('UPDATE user SET score_total = score_total + current_score, current_score = 0, games_played = games_played + 1 WHERE name = :username');
-            $stmt->execute(array(':username' => $username['name']));
+            $stmt->execute(array(':username' => $player));
         }
         //Redirect back to the actions page
         header("Location: http://php-tsorenson.rhcloud.com/Database/actions.php");
